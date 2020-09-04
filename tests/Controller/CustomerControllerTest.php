1a5e86bdb6a245f01578ad716dded8271f97bcff
LEVEQUE <alexis.leveque78@gmail.com>
2020-09-04 13:58:45 +0200

    test for ListCustomers is ok #34

--- tests/Controller/CustomerControllerTest.php
+++ tests/Controller/CustomerControllerTest.php
@@ -6,5 +6,24 @@ namespace App\Tests\Controller;
 
 class CustomerControllerTest extends AuthenticatedClient
 {
+    public function testListCustomers()
+    {
+        $client = self::createClient();
 
+        $client = $this->createAuthenticatedClient($client);
+
+        $client->request('GET',"/api/customers");
+
+        $data = json_decode($client->getResponse()->getContent(),'true');
+
+        $this->assertIsArray($data);
+        $this->assertJson($client->getResponse()->getContent());
+        $this->assertArrayHasKey('_links',$data);
+        $this->assertArrayHasKey('_embedded',$data);
+
+        $dataEmbedded = $data["_embedded"];
+        $items = $dataEmbedded["items"];
+
+        $this->assertCount(10, $items);
+    }
 }
