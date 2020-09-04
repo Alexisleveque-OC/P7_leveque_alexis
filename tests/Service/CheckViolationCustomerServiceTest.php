b0db0f32b243aea202d36a5182ca7f9405ac9767
LEVEQUE <alexis.leveque78@gmail.com>
2020-09-04 15:14:44 +0200

    CheckViolationCustomerServiceTest.php is ok #34

---
+++ tests/Service/CheckViolationCustomerServiceTest.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Tests\Service;
+
+
+use App\Service\CheckViolationCustomerService;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+class CheckViolationCustomerServiceTest extends TestCase
+{
+    /**
+     * @var CheckViolationCustomerService
+     */
+    private CheckViolationCustomerService $checkViolationCustomerService;
+    private $violationList;
+
+    public function setUp()
+    {
+        $this->checkViolationCustomerService = new CheckViolationCustomerService();
+
+        $violation = $this->createMock(ConstraintViolation::class);
+        $this->violationList = $this->createMock(ConstraintViolationList::class);
+//        $violationList->add($violation);
+        $this->violationList->method('count')
+            ->willReturn(1);
+
+        return $this->violationList;
+    }
+
+    public function testCheckViolation()
+    {
+
+//        $this->expectException('App\Exception\ResourceValidationException');
+        $this->expectException('\Exception');
+        $this->checkViolationCustomerService->checkViolation($this->violationList);
+
+    }
+}
